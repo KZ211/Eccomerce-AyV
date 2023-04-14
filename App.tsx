f21d44b98067f44a924980ad6ee2bffb04c6d787
KZ211 <62577028+KZ211@users.noreply.github.com>
2023-04-14 16:00:32 -0300

    Layout created

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Articulos from "./pages/Articulos";
+
 import './style.css';
 
 export default function App() {
   return (
     <div>
-      
+      <Articulos />
     </div>
   );
 }
